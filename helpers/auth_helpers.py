2ef0a21e9ed91b8780fa4c7f7e17fb395f4acba7
abhinna1 <manandharabhinna@gmail.com>
2023-06-03 11:16:38 +0545

    common passwords check and password hash added.

---
+++ helpers/auth_helpers.py
@@ -0,0 +1,52 @@
+import re
+import bcrypt
+import csv
+
+
+def check_common_password(password):
+    """
+    Checks if the password exists in the common password CSV file, considering case changes
+    and combinations of multiple common passwords.
+    Returns True if the password is found, False otherwise.
+    """
+    with open('commons/common_passwords.csv', 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            common_password = row['password']
+            # Check for exact match
+            if common_password == password:
+                return True
+            # Check for case-insensitive match
+            if common_password.lower() == password.lower():
+                return True
+    return False
+
+
+def verify_password_strength(password):
+    """
+    Verifies that a password meets the complexity requirements and is not a common password.
+    Returns True if the password is strong, False otherwise.
+    """
+    # Verify password length
+    if not (8 <= len(password) <= 12):
+        return False
+
+    # Verify password complexity
+    if not re.search(r'[A-Z]', password):  # Check for uppercase letters
+        return False
+    if not re.search(r'[a-z]', password):  # Check for lowercase letters
+        return False
+    if not re.search(r'\d', password):  # Check for numbers
+        return False
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):  # Check for special characters
+        return False
+
+    return True
+
+
+def hash_password(password:str) -> str:
+    """
+    Hashes a password using the bcrypt algorithm.
+    Returns the hashed password as a string.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
--- services/user_services.py
+++ services/user_services.py
@@ -1,7 +1,7 @@
 from repositories.UserRepository import UserRepository
 from sqlalchemy.orm import Session
 from models.User import User
-
+from helpers.auth_helpers import verify_password_strength, hash_password, check_common_password
 class UserServices:
     def __init__(self, db:Session):
         self.user_repository = UserRepository(db)
@@ -9,5 +9,15 @@ class UserServices:
     def create_user(self, username: str, email: str, password: str, cfm_password:str)-> User:
         if password != cfm_password:
             raise Exception('Password and confirm password are not match')
+        if self.user_repository.get_user_by_username(username):
+            raise Exception('Username already exists')
+        if self.user_repository.get_user_by_email(email):
+            raise Exception('Email already exists')
+        
+        if check_common_password(password):
+            raise Exception('Password is too common.')
+        if not verify_password_strength(password):
+            raise Exception('Password is too weak')
+        
+        password = hash_password(password)
         return self.user_repository.create_user(username, email, password)
-    
